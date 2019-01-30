70332c56f2e7f86b24370d8dc6e3b9a6beda1756
jalharbi <jumanah.alharbi92@gmail.com>
2019-01-30 12:15:19 +0300

    .

---
+++ my-app-rateme/components /ProductForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ProductForm extends Component {
+    constructor(){
+        super();
+        this.state ={
+        }
+    }
+    render(){
+        return (
+            <div></div>
+            )
+        }
+    }
+    
+    export default ProductForm;
