734abc98c47bcdd3b46b974894b462de1f828b27
jalharbi <jumanah.alharbi92@gmail.com>
2019-01-30 17:00:13 +0300

    display the product

--- my-app-rateme/src/components /Tile.js
+++ my-app-rateme/src/components /Tile.js
@@ -4,8 +4,19 @@ import React from 'react';
 const Tile = (props)=> {
 
     return(
-        <div></div>
+        <div className='tile'>
+<h2>{props.product.name}</h2>
+<h2>{props.product.rating}</h2>
+<h2>{props.product.brand}</h2>
+<h2>{props.product.description}</h2>
+<h2>{props.product.expected_price}</h2>
+<h2>{props.product.color}</h2>
+<h2>{props.product.model_year}</h2>
+<img src={props.product.image} alt="" />
+
+          </div>
     )
+
 }
 
 export default Tile;
